0f38a9d299dc542515934c8236904425799ffdf4
Kevin <kevinsevinche@gmail.com>
2021-07-21 15:33:53 -0500

    Add test for the localStorage

--- src/addrm.test.js
+++ src/addrm.test.js
@@ -1,27 +1,42 @@
 /**
  * @jest-environment jsdom
  */
-jest.mock('./addrm');
-import AddRm from './addrm.js';
+jest.mock("./addrm");
+import AddRm from "./addrm.js";
 
-describe('Testing adding and deleting <li> in the HTML', () => {
-    test('Add <li> to <ul> test', () => {
-        document.body.innerHTML =
-        '<div>' +
-        '  <ul id="td-section"></ul>' +
-        '</div>';
-        const add = new AddRm();
-        add.addToList();
-        add.addToList();
-        const list = document.querySelectorAll('.td-item');
-        expect(list).toHaveLength(2);
-    });
-    
-    test('Removes first <li> from <ul> test', () => {
-        const add = new AddRm();
-        add.removeLine();
-        add.removeLine();
-        const list = document.querySelectorAll('.td-item');
-        expect(list).toHaveLength(0);
-    });
+describe("Testing adding and deleting <li> in the HTML", () => {
+  test("Add <li> to <ul> test", () => {
+    document.body.innerHTML =
+      "<div>" + '  <ul id="td-section"></ul>' + "</div>";
+    const add = new AddRm();
+    add.addToList();
+    add.addToList();
+    const list = document.querySelectorAll(".td-item");
+    expect(list).toHaveLength(2);
+  });
+
+  test("Removes first <li> from <ul> test", () => {
+    const add = new AddRm();
+    add.removeLine();
+    add.removeLine();
+    const list = document.querySelectorAll(".td-item");
+    expect(list).toHaveLength(0);
+  });
+});
+
+describe("Testing adding and deleting object in LocalStorage", () => {
+  test("Add object to localStorage array", () => {
+    const add = new AddRm();
+    add.addToList();
+    add.addToList();
+    const localStorageArray = JSON.parse(localStorage.getItem("ToDoList"));
+    expect(localStorageArray).toHaveLength(2);
+  });
+
+  test("Delete object from localStorage array", () => {
+    const add = new AddRm();
+    add.removeLine();
+    const localStorageArray = JSON.parse(localStorage.getItem("ToDoList"));
+    expect(localStorageArray).toHaveLength(1);
+  });
 });
