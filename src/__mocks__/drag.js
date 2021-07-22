843c08072f0624c65fa022a7558862b0d453d23e
Kevin <kevinsevinche@gmail.com>
2021-07-22 12:12:58 -0500

    Add test for drag and drop

---
+++ src/__mocks__/drag.js
@@ -0,0 +1,41 @@
+import Status from "./status.js";
+
+const container = document.querySelector("#td-section");
+
+export default class DragDropSort {
+  dragOver = () => {
+    const container = document.querySelector("#td-section");
+
+    // e.preventDefault();
+    const draggable = document.querySelector("#task-1");
+    // const draggableElements = [
+    //   ...container.querySelectorAll(".td-item:not(.dragging)"),
+    // ];
+    const afterElement = document.querySelector("#task-3");
+    if (afterElement == null) {
+      container.appendChild(draggable);
+    } else {
+      container.insertBefore(draggable, afterElement);
+    }
+  };
+
+  dropSort = () => {
+    // e.preventDefault();
+    const array = document.getElementById("td-section").childNodes;
+    const len = array.length;
+    const tdList = JSON.parse(localStorage.getItem("ToDoList"));
+    for (let h = 0; h < len; h += 1) {
+      array[h].id = h + 1;
+      for (let y = 0; y < len; y += 1) {
+        const text =
+          array[h].firstChild.nextSibling.nextSibling.nextSibling.innerHTML;
+        if (text === tdList[y].description) {
+          tdList[y].index = array[h].id;
+        }
+      }
+    }
+    tdList.sort((x, y) => x.index - y.index);
+    const status = new Status();
+    status.saveStorage();
+  };
+}
--- src/testpart2.test.js
+++ src/testpart2.test.js
@@ -1,36 +1,67 @@
 /**
  * @jest-environment jsdom
  */
- import { expect } from '@jest/globals';
-import AddRm from './addrm.js';
- import Status from './status.js';
+import { expect } from "@jest/globals";
+import AddRm from "./addrm.js";
+import Status from "./status.js";
+import DragDropSort from "./drag.js";
 
- jest.mock('./addrm');
- jest.mock('./status');
- 
- describe('Edit test', () => {
-     test('edit test', () => {
-        document.body.innerHTML = `<div>
+jest.mock("./addrm");
+jest.mock("./status");
+jest.mock("./drag");
+
+describe("Edit test", () => {
+  test("edit test", () => {
+    document.body.innerHTML = `<div>
             <ul id="td-section">
             </ul>
         </div>`;
-        const addrm = new AddRm();
-        addrm.addToList();
-        addrm.editText();
-        var text = document.querySelector('.td-item').firstChild.nextSibling.nextSibling.nextSibling.innerHTML;
-        const localStorageArray = JSON.parse(localStorage.getItem('ToDoList'));
-        expect(localStorageArray[0].description).toBe('new text');
-        expect(text).toBe('new text');
-     });
- });
+    const addrm = new AddRm();
+    addrm.addToList();
+    addrm.editText();
+    var text =
+      document.querySelector(".td-item").firstChild.nextSibling.nextSibling
+        .nextSibling.innerHTML;
+    const localStorageArray = JSON.parse(localStorage.getItem("ToDoList"));
+    expect(localStorageArray[0].description).toBe("new text");
+    expect(text).toBe("new text");
+  });
+});
 
- describe("'Completed' status test", () => {
-     test('test completed status', () => {
-        const status = new Status();
-        var localStorageArray = JSON.parse(localStorage.getItem('ToDoList'));
-        expect(localStorageArray[0].completed).toBeFalsy();
-        status.validation();
-        var localStorageArray = JSON.parse(localStorage.getItem('ToDoList'));
-        expect(localStorageArray[0].completed).toBeTruthy();
-     });
- });
+describe("'Completed' status test", () => {
+  test("test completed status", () => {
+    const status = new Status();
+    var localStorageArray = JSON.parse(localStorage.getItem("ToDoList"));
+    expect(localStorageArray[0].completed).toBeFalsy();
+    status.validation();
+    var localStorageArray = JSON.parse(localStorage.getItem("ToDoList"));
+    expect(localStorageArray[0].completed).toBeTruthy();
+  });
+});
+
+describe("'Completed' status test", () => {
+  test("test completed status", () => {
+    document.body.innerHTML = `<div>
+            <ul id="td-section">
+              <div class="td-item" id='task-1' draggable="true">
+                          <input type="checkbox" class="checkbox"/>
+                          <p contenteditable="true">Walk the dog</p>
+                          <i class="delete-line me-2 far fa-trash-alt"></i>
+                          <i class="drag-line fas fa-ellipsis-v"></i>
+              </div>
+              <div class="td-item" id='task-2' draggable="true">
+                          <input type="checkbox" class="checkbox"/>
+                          <p contenteditable="true">Wash dishes</p>
+                          <i class="delete-line me-2 far fa-trash-alt"></i>
+                          <i class="drag-line fas fa-ellipsis-v"></i>
+              </div>
+              <div class="td-item" id='task-3' draggable="true">
+                          <input type="checkbox" class="checkbox"/>
+                          <p contenteditable="true">Cook the food</p>
+                          <i class="delete-line me-2 far fa-trash-alt"></i>
+                          <i class="drag-line fas fa-ellipsis-v"></i>
+              </div>
+            </ul>
+        </div>`;
+  });
+});
